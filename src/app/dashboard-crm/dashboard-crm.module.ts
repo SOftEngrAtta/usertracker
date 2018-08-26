7cd6e07b1b57d7964e8392f86396bd2fc712c43b
atta_ur_rehman <atta.rehman8123@gmail.com>
2018-08-26 18:32:38 +0500

    working on usertracker

---
+++ src/app/dashboard-crm/dashboard-crm.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardCrmComponent } from './dashboard-crm.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+
+export const appRoutes: Routes = [
+    { path: '', component: DashboardCrmComponent },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(appRoutes),
+    FlexLayoutModule,
+    MatCardModule
+  ],
+  declarations: [DashboardCrmComponent],
+  exports: [ ]
+})
+export class DashboardCrmModule { }
